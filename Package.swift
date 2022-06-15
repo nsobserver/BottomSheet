d0b52c82ffedf28a3455103cb9e914a3c82bc2ac
nsobserver <78545642+nsobserver@users.noreply.github.com>
2022-06-15 15:10:18 +0200

    Fix Package Platform (#3)

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     name: "FINNBottomSheet",
     defaultLocalization: "en",
     platforms: [
-        .iOS(.v11)
+        .iOS(.v13)
     ],
     products: [
         .library(name: "FINNBottomSheet", targets: ["FINNBottomSheet"])
